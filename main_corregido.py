5253d7b53e2eb64b7a7c537fae1e743ebf060768
Mau <mauricio.gonzalezt@alumno.buap.mx>
2024-10-02 12:52:35 -0600

    añadiendo correcciones y kmeans por terminar

--- main_corregido.py
+++ main_corregido.py
@@ -31,27 +31,26 @@ def heom_distance(x1, x2, tipos_datos):
     dist = 0
     for i in range(len(x1)):
         # Si alguna de las instancias tiene un valor faltante, sumamos 1
-        if pd.isna(x1.iloc[i]) or pd.isna(x2[i]):
+        if pd.isna(x1[i]) or pd.isna(x2[i]):
             dist += 1
             print("\nValor faltante.")
         else:
             if tipos_datos[i]:  # Categórico
-                if x1.iloc[i] != x2.iloc[i]:
+                if x1[i] != x2[i]:
                     dist += 1
-                    print(f"\nDistancia categorica de {x1.iloc[i]} a {x2.iloc[i]} = {1}") #DEL
+                    print(f"\nDistancia categorica de {x1[i]} a {x2[i]} = {1}") #DEL
                 else:                                                                     #DEL
-                    print(f"\nDistancia categorica de {x1.iloc[i]} a {x2.iloc[i]} = {0}") #DEL 
+                    print(f"\nDistancia categorica de {x1[i]} a {x2[i]} = {0}") #DEL 
             else:  # Continuo
                 try: 
-                    temp= (float(x1.iloc[i]) - float(x2.iloc[i])) ** 2 # Aseguramos que las variables continuas sean numéricas
+                    temp= (float(x1[i]) - float(x2[i])) ** 2 # Aseguramos que las variables continuas sean numéricas
                     dist += temp  
-                    print(f"\nDistancia continua de {x1.iloc[i]} a {x2.iloc[i]} = {temp}")  #DEL
+                    print(f"\nDistancia continua de {x1[i]} a {x2[i]} = {temp}")  #DEL
                 except ValueError:
-                    dist += 1 if x1.iloc[i] != x2.iloc[i] else 0
+                    dist += 1 if x1[i] != x2[i] else 0
     return np.sqrt(dist)
 
 # 5. Implementación del K-Means con HEOM sin modificar datos
-# 4. Implementación del K-Means con HEOM
 def kmeans_heom(X, k=2, max_iters=100):
     # Elegir dos instancias aleatorias como centroides iniciales
     centroids = X.sample(n=k).values
